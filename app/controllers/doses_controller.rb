2a2ffc40611698046f0012fb93db0f487ce6deba
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-04-16 15:32:00 +0200

    Controllers ok

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,7 +1,36 @@
 class DosesController < ApplicationController
   def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @doses = Dose.all
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+
+    respond_to do |format|
+      if @dose.save
+        format.html { redirect_to @cocktail, notice: 'dose was successfully created.' }
+        format.json { render :show, status: :created, location: @cocktail }
+      else
+        format.html { render :new }
+        format.json { render json: @dose.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(cocktail)
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
