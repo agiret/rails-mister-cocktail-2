b90aa51f92ff73da9c5ce00b59c94002f18a9810
Antoine GIRET <antoinegiret1-github@yahoo.fr>
2018-04-16 15:10:16 +0200

    New cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -13,10 +13,25 @@ class CocktailsController < ApplicationController
     @cocktail = Cocktail.new
   end
 
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    # respond_to do |format|
+      if @cocktail.save
+        # format.html { redirect_to @cocktail, notice: 'cocktail was successfully created.' }
+        # format.json { render :show, status: :created, location: @cocktail }
+        redirect_to @cocktail # ou root_path mais rectif pour rake
+      else
+        # format.html { render :new }
+        # format.json { render json: @cocktail.errors, status: :unprocessable_entity }
+        render :new
+      end
+    # end
+  end
+
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :photo)
+    params.require(:cocktail).permit(:name)
   end
 
 end
